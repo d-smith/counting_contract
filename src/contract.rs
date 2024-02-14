2bf1063d687e0fdc47839633b775205b8e58650e
d-smith <doug.smith.mail@gmail.com>
2024-02-14 13:42:11 -0800

    Added query

---
+++ src/contract.rs
@@ -0,0 +1,8 @@
+ 
+pub mod query {
+    use crate::msg::ValueResp;
+ 
+    pub fn value() -> ValueResp {
+        ValueResp { value: 0 }
+    }
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,9 @@
 use cosmwasm_std::{
-    entry_point, Binary, Deps, DepsMut, Empty, Env, MessageInfo, Response, StdResult,
+    entry_point, to_binary, Binary,  Deps, DepsMut, Empty, Env, MessageInfo, Response, StdResult,
 };
+
+mod contract;
+pub mod msg;
 
 #[entry_point]
 pub fn instantiate(
@@ -8,40 +11,15 @@ pub fn instantiate(
     _env: Env, 
     _info: MessageInfo, 
     _msg: Empty) -> StdResult<Response> {
-    Ok(Response::default())
+    Ok(Response::new())
 }
 
 #[entry_point]
-pub fn execute(
-    _deps: DepsMut,
-    _env: Env,
-    _info: MessageInfo,
-    _msg: Empty,
-) -> StdResult<Response> {
-    Ok(Response::default())
-}
-
-#[entry_point]
-pub fn query(
-    _deps: Deps,
-    _env: Env,
-    _msg: Empty,
-) -> StdResult<Binary> {
-    Ok(Binary::default())
-}
-
-
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+pub fn query(_deps: Deps, _env: Env, msg: msg::QueryMsg) -> StdResult<Binary> {
+    use msg::QueryMsg::*;
+    use contract::query;
+ 
+    match msg {
+        Value {} => to_binary(&query::value()),
     }
 }
