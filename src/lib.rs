579bc50e6cd6f4c9a055d8fbbcc4bfb7404115da
d-smith <doug.smith.mail@gmail.com>
2024-02-15 13:46:34 -0800

    Added contract tests

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,5 @@
 use cosmwasm_std::{
-    entry_point, to_binary, Binary,  Deps, DepsMut, Empty, Env, MessageInfo, Response, StdResult,
+    entry_point, to_binary, Binary, Deps, DepsMut, Empty, Env, MessageInfo, Response, StdResult,
 };
 
 mod contract;
@@ -7,19 +7,91 @@ pub mod msg;
 
 #[entry_point]
 pub fn instantiate(
-    _deps: DepsMut, 
-    _env: Env, 
-    _info: MessageInfo, 
-    _msg: Empty) -> StdResult<Response> {
+    _deps: DepsMut,
+    _env: Env,
+    _info: MessageInfo,
+    _msg: Empty,
+) -> StdResult<Response> {
     Ok(Response::new())
 }
 
 #[entry_point]
 pub fn query(_deps: Deps, _env: Env, msg: msg::QueryMsg) -> StdResult<Binary> {
-    use msg::QueryMsg::*;
     use contract::query;
- 
+    use msg::QueryMsg::*;
+
     match msg {
         Value {} => to_binary(&query::value()),
+        Incremented { value } => to_binary(&query::incremented(value)),
+    }
+}
+
+// Also need to add the execute entry point to use multitest
+#[entry_point]
+pub fn execute(_deps: DepsMut, _env: Env, _info: MessageInfo, _msg: Empty) -> StdResult<Response> {
+    Ok(Response::new())
+}
+
+#[cfg(test)]
+mod test {
+    use cosmwasm_std::{Addr, Empty};
+    use cw_multi_test::{App, Contract, ContractWrapper, Executor};
+
+    use crate::msg::{QueryMsg, ValueResp};
+    use crate::{execute, instantiate, query};
+
+    fn counting_contract() -> Box<dyn Contract<Empty>> {
+        let contract = ContractWrapper::new(execute, instantiate, query);
+        Box::new(contract)
+    }
+
+    #[test]
+    fn query_value() {
+        let mut app = App::default();
+
+        let contract_id = app.store_code(counting_contract());
+
+        let contract_addr = app
+            .instantiate_contract(
+                contract_id,
+                Addr::unchecked("sender"),
+                &Empty {},
+                &[],
+                "Counting contract",
+                None,
+            )
+            .unwrap();
+
+        let resp: ValueResp = app
+            .wrap()
+            .query_wasm_smart(contract_addr, &QueryMsg::Value {})
+            .unwrap();
+
+        assert_eq!(resp, ValueResp { value: 0 });
+    }
+
+    #[test]
+    fn query_incremented() {
+        let mut app = App::default();
+
+        let contract_id = app.store_code(counting_contract());
+
+        let contract_addr = app
+            .instantiate_contract(
+                contract_id,
+                Addr::unchecked("sender"),
+                &Empty {},
+                &[],
+                "Counting contract",
+                None,
+            )
+            .unwrap();
+
+        let resp: ValueResp = app
+            .wrap()
+            .query_wasm_smart(contract_addr, &QueryMsg::Incremented { value: 1 })
+            .unwrap();
+
+        assert_eq!(resp, ValueResp { value: 2 });
     }
 }
